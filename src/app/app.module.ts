b64ea6d1ee7b52f850f6dc186a4674348eec1dac
Pau Franco <paufraba@gmail.com>
2018-09-19 13:23:35 +0200

    Sección 2, vídeo 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        PortfolioComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styles: []
+})
+export class PortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
