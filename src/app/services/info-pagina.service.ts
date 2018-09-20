1e04256b39921ce4f33d992453e958d0f16a8a61
Pau Franco <paufraba@gmail.com>
2018-09-20 09:49:59 +0200

    Sección 5, vídeo 34

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,15 +9,34 @@ export class InfoPaginaService {
 
     info: InfoPagina = {};
     cargada = false;
+    equipo: any;
+    equipoCargado = false;
 
     constructor(private http: HttpClient) {
+        this.cargarInfo();
+        this.cargaEquipo();
+    }
+
+    private cargarInfo() {
         this.cargada = false;
 
         this.http.get('assets/datos/datos-globales.json')
             .subscribe((data: InfoPagina) => {
-                console.log(data);
+                // console.log(data);
                 this.cargada = true;
                 this.info = data;
+            });
+
+    }
+
+    private cargaEquipo() {
+        this.equipoCargado = false;
+
+        this.http.get('https://porfolio-df0ac.firebaseio.com/equipo.json')
+            .subscribe((data: any[]) => {
+                console.log(data);
+                this.equipoCargado = true;
+                this.equipo = data;
             });
     }
 }
